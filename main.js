5c0c7e44531a3dff908a38d789294a79071ed4f1
Harsh Tomar <harshtomar6@gmail.com>
2017-09-01 14:28:54 +0530

    Changed default PORT number

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let mainWindow = null
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 1100, height: 650, backgroundColor: '#2A363B', show: false})
   mainWindow.setMenu(null);
-  mainWindow.loadURL('http://localhost:3000/')
+  mainWindow.loadURL('http://localhost:62000/')
   mainWindow.webContents.openDevTools();
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
