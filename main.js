63d5e5e2b8a8d2114f275a71594c34bc57a97602
Harsh Tomar <harshtomar6@gmail.com>
2017-09-14 18:58:12 +0530

    Fixed scrollbar for macOS

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const {app, BrowserWindow } = electron
 let mainWindow = null
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 1100, height: 650, backgroundColor: '#2A363B', show: false, titleBarStyle: "hiddenInset"})
+  mainWindow = new BrowserWindow({width: 1100, height: 650, backgroundColor: '#2A363B', show: false})
   mainWindow.setMenu(null);
   mainWindow.loadURL('http://localhost:62000/')
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
     mainWindow.focus()
